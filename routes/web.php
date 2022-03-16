a20f253bb2cdc716d49505b96e6b4cd8cb823c0c
antlogist <antlogist@gmail.com>
2022-03-16 23:09:36 +0300

    Resource routing fixed

--- routes/web.php
+++ routes/web.php
@@ -45,9 +45,11 @@ Route::get('/map',
     [\App\Http\Controllers\MapController::class,
     'show'])->middleware(['auth', 'verified'])->name('map');
 
-Route::get('/categories',
-    [\App\Http\Controllers\CategoryController::class,
-    'index'])->middleware(['auth', 'verified'])->name('categories');
+Route::resource('/categories', \App\Http\Controllers\CategoryController::class)
+    ->middleware(['auth', 'verified'])->name('index', 'categories');
+
+// Route::resource('/categories', \App\Http\Controllers\CategoryController::class)->middleware(['auth', 'verified'])->name('categories');
+// Route::resource('posts', PostsController::class)->except('index', 'show');
 
 // Route::inertia('/welcome', 'Welcome');
 

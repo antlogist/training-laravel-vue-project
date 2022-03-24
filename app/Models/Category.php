093aa25ff09677730a7749f7bd0e6c1146d8ae95
antlogist <antlogist@gmail.com>
2022-03-24 22:30:37 +0300

    Subcat relation added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -24,4 +24,8 @@ class Category extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function subcategories() {
+        return $this->hasMany(Subcategory::class);
+    }
 }

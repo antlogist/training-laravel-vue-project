412e48eab72868e860013885ea936507337c351e
antlogist <antlogist@gmail.com>
2022-04-10 12:19:07 +0300

    Map model and seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(SubcategorySeeder::class);
         $this->call(NoteSeeder::class);
+        $this->call(MapSeeder::class);
     }
 }
